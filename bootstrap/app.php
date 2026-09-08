4934bcb85f0cf3f67c9cc5ba9af2b2cf87815e70
thanhnam1122 <117763613+thanhnam1122@users.noreply.github.com>
2026-09-08 13:53:22 +0700

    Fix useStoragePath call on Application instance after configure create

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$builder = Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,12 +15,9 @@ $builder = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    });
+    })->create();
 
 // Use writable /tmp/storage on Vercel Serverless
-if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || getenv('VERCEL') || getenv('APP_STORAGE')) {
-    $storagePath = getenv('APP_STORAGE') ?: '/tmp/storage';
-    $builder->withStoragePath($storagePath);
-}
+$app->useStoragePath(env('APP_STORAGE', '/tmp/storage'));
 
-return $builder->create();
+return $app;
